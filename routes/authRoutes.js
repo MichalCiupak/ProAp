0654e1e815f5e1bd8c5417fc134cade2b82ab270
DENYS FOKASHCHUK <denys.fokashchuk@gmail.com>
2022-12-03 18:29:17 +0100

    Updated router routes/authRouter.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,9 @@
+const { login, register } = require("../controlers/authController");
+
+const express = require("express");
+const router = express.Router();
+
+router.route("/login").post(login);
+router.route("/register").post(register);
+
+module.exports = router;
