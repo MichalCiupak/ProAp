4d3371e91e25cc00b8639c3d05a3252a39956f13
dfokashc <01174305@pw.edu.pl>
2022-12-10 17:23:16 +0100

    d

--- public/js/fillfield.js
+++ public/js/fillfield.js
@@ -3,21 +3,19 @@ var surname = document.getElementById("surname");
 var address = document.getElementById("adres");
 var email = document.getElementById("email");
 var login = document.getElementById("login");
-
-window.addEventListener("load", async (e)=>
-{
-    let data = await axios.get("/api/profile",
-    {
-        headers: {
-            authorization: localStorage.getItem("token")
-        },
-    });
-    const UserData = data.data.user;
-    namex.innerHTML = UserData.name;
-    surname.innerHTML = UserData.surname;
-    address.innerHTML = UserData.address;
-    email.innerHTML = UserData.email;
-    login.innerHTML = UserData.login;
-
-
-})
+console.log(email);
+window.addEventListener("load", async (e) => {
+  let data = await axios.get("/api/profile", {
+    headers: {
+      authorization: localStorage.getItem("token"),
+    },
+  });
+  //   console.log(data.data);
+  const UserData = await data.data;
+  console.log(UserData);
+  namex.innerHTML = UserData.name;
+  surname.innerHTML = UserData.surname;
+  address.innerHTML = UserData.address;
+  email.innerHTML = UserData.email;
+  login.innerHTML = UserData.login;
+});
