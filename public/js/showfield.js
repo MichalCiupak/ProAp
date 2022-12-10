d1e559f45d254e680a736b7c5e2afa4b9d663edf
pkutyla <01168894@pw.edu.pl>
2022-12-10 16:26:26 +0100

    Update showfield.js

--- public/js/showfield.js
+++ public/js/showfield.js
@@ -1,5 +1,3 @@
-// document.getElementById("fff").innerHTML = ((window.location.href).split("/"))[((window.location.href).split("/")).length -1]+"x"
-
 document.getElementById(((window.location.href).split("/"))[((window.location.href).split("/")).length -1]+"x").style.backgroundColor = "#00008B";
 document.getElementById(((window.location.href).split("/"))[((window.location.href).split("/")).length -1]+"s").onmouseover = function()
 {
