1177abcddb172a8af5dbeb462bc29ddf711dc8ab
dfokashc <01174305@pw.edu.pl>
2022-12-16 16:34:43 +0100

    Added functionality of adding product to cart using single product page

--- public/js/singleProduct.js
+++ public/js/singleProduct.js
@@ -1,16 +1,15 @@
 const { host, hostname, href, origin, pathname, port, protocol, search } =
   window.location;
 const productID = pathname.slice("/singleProduct/".length);
-console.log(pathname);
-console.log(productID);
-console.log(pathname.indexOf("/singleProduct/"));
-
+// console.log(pathname);
+// console.log(productID);
+// console.log(pathname.indexOf("/singleProduct/"));
 const loadingText = document.querySelector(".loading-text");
 const errorText = document.querySelector(".error-text");
 const productWrapper = document.querySelector(".product-wrapper");
 
-console.log(2);
-
+let currentUser = null;
+let hasValidToken = true;
 window.addEventListener("DOMContentLoaded", start);
 
 async function start() {
@@ -34,16 +33,56 @@ async function start() {
     ${product.description}
     </p>
     <div class="btn-container">
-    <button class="add-to-cart-btn">Add To Cart</button>
+    <button class="add-to-cart-btn" data-id=${product._id}>Add To Cart</button>
     <a href="/">
       <button class="btn">Go Back</button>
     </a>
   </div>  `;
+    const addToCartBtn = document.querySelector(".add-to-cart-btn");
+    addToCartBtn.addEventListener("click", async function () {
+      await checkToken();
+      if (hasValidToken && currentUser) {
+        addProductToLocalStorage(this.dataset.id);
+      } else {
+        console.log(1);
+        window.location.href = "/login";
+      }
+    });
   } catch (error) {
     displayErrorText();
     hideLoading();
   }
   hideLoading();
+}
+
+function addProductToLocalStorage(productID) {
+  let cart_value = JSON.parse(localStorage.getItem("cart"));
+  if (!cart_value.includes(productID)) {
+    cart_value.push(productID);
+    localStorage.setItem("cart", JSON.stringify(cart_value));
+  }
+}
+
+async function checkToken() {
+  const tokenIsPresent = localStorage.getItem("token") ? true : false;
+  if (tokenIsPresent) {
+    try {
+      const headers = {
+        authorization: localStorage.getItem("token"),
+      };
+      const getUserData = await axios.get("/api/profile", { headers });
+      currentUser = getUserData.data;
+      hasValidToken = true;
+      if (localStorage.getItem("cart") === null) {
+        localStorage.setItem("cart", "[]");
+      }
+    } catch (error) {
+      console.log(error);
+      hasValidToken = false;
+      currentUser = null;
+      localStorage.clear();
+    }
+  }
 }
 
 function displayLoading() {
