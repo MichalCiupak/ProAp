6a49301ebfba3bcd8c066f9411fd243cf9290fe7
Michal Ciupak <michal.ciupak0@gmail.com>
2023-01-08 01:29:50 +0100

    dark theme

--- public/js/darkTheme.js
+++ public/js/darkTheme.js
@@ -4,9 +4,11 @@ check.addEventListener("change", function()
     if(check.checked == true)
     {
         document.getElementById("toggleinfo").innerHTML = "Dark"
+        document.body.classList.toggle("dark-ini")
     }
     else
     {
-        document.getElementById("toggleinfo").innerHTML = "Light"  
+        document.getElementById("toggleinfo").innerHTML = "Light"
+        document.body.classList.toggle("dark-ini")
     }
 })
